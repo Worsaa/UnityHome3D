2217fb5c6b8a88e124a3b894ceac6659a875d4fb
Worsaa <jack.hellstaf@gmail.com>
2023-01-30 00:47:26 +0100

    3d ws2
    :)

---
+++ UnityHome3D/Assets/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        movement.Normalize();
+        transform.Translate(movement * 3.0f * Time.deltaTime);
+    }
+}
